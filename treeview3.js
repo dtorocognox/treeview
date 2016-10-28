ff716a5511a7aca6ddd0f7ec6738717ea6f9739b
David Toro Alzate <dtoro@cognox.com>
2016-10-28 09:50:59 -0500

    treview casi terminado, problemas con Don papá

--- treeview3.js
+++ treeview3.js
@@ -1,4 +1,4 @@
-﻿var $suLu = $('<ul><input type="checkbox">Don Papá<span> - </span></ul>');
+﻿var $suLu = $('<ul class="treeview"><input type="checkbox" id="suLu">Don Papá<span> - </span></ul>');
 
 $.getJSON("Tree.json", function($data){
 
@@ -10,7 +10,7 @@ $.getJSON("Tree.json", function($data){
     function draw($data) {
         var $ul = $('<ul></ul>').append('<input type="checkbox">' + $data.name );
         if($data.sons.length > 0){
-            $ul.append('<span> - </span>');
+            $ul.append('<span> - 🔎 </span>');
             for (var i = 0; i<$data.sons.length; i++){
                 $ul.append(draw($data.sons[i]));
             }
@@ -29,17 +29,33 @@ $.getJSON("Tree.json", function($data){
     });
     $('input').click(function () {
         var $this = $(this);
-        $this.parent().find('input').prop('checked', $this.is(':checked'));
-        if($this.parent().find('input').is(':checked')){
-            $this.parent().prop('checked', true);
-        }
-        console.log($this.parent().parent().find('input').not('input:first').prop('checked'));
-        console.log($this.parent().parent().find('input').not('input:first'));
-        console.log($this.parent().parent().children());
-        if($this.parent().find().is(':checked')){
-            console.log("si");
-        }
+        $this.parent().find('input').prop('checked', $this.is(':checked')).prop('indeterminate', false);
+        cont($this);
+
     });
 
+    function cont($this) {
+        var inputVector = $this.parent().parent().find('input').not('input:first');
+        var $checkBoxMaster = $this.parent().parent().find('input:first');
+        var conter = 0;
+        for(i=0; i < inputVector.length; i++){
+            if($(inputVector[i]).prop('checked')){
+                conter++;
+            }
+            if(conter>0){
+                $checkBoxMaster.prop('indeterminate', true);
+            }else{
+                $checkBoxMaster.prop('indeterminate', false);
+            }
+        }
+        if (conter == inputVector.length){
+            $checkBoxMaster.prop('indeterminate', false).prop('checked', true);
+        }else{
+            $checkBoxMaster.prop('checked', false);
+        }
+        if(!$($this.parent().parent()).hasClass('treeview') ){
+            cont($($checkBoxMaster));
+        }
+    }
 });
 
